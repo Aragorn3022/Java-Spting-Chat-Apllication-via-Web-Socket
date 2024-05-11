a67dff50110fe455f9e4a42c6640e9285268e284
Aswad Yousaf <ahmadyousaf7white@hotmail.com>
2024-05-11 23:32:02 +0500

    Trying Something

---
+++ src/test/java/com/example/oppproj/OppProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oppproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OppProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
